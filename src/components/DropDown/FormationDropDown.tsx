998039f8ad47a950370615113392f7346a0e5c75
Rutle <rutle4@gmail.com>
2021-06-01 20:51:51 +0300

    Clean up formation dropdown.

--- src/components/DropDown/FormationDropDown.tsx
+++ src/components/DropDown/FormationDropDown.tsx
@@ -1,8 +1,135 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import { Dropdown, useDropdownMenu, useDropdownToggle } from 'react-overlays';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '_/reducers/rootReducer';
 import { selectFormation } from '_/reducers/slices/formationGridSlice';
-import DropDownButton from './DropDownButton';
+import { CaretLeft } from '../Icons';
+
+interface FormationDropDownProps {
+  listData: string[] | undefined;
+  themeColor: string;
+  selectedIdx: number;
+  selectIndex: (idx: number) => void;
+  onSelect: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const DropDownMenu: React.FC<FormationDropDownProps> = ({
+  listData,
+  themeColor,
+  selectedIdx,
+  selectIndex,
+  onSelect,
+}) => {
+  const [menuProps, { show }] = useDropdownMenu({
+    flip: true,
+    offset: [0, 8],
+  });
+  return (
+    <div
+      {...menuProps}
+      role="menu"
+      className={`formation-dropdown-menu ${themeColor}`}
+      style={{
+        opacity: `${show ? '1' : '0'}`,
+      }}
+    >
+      {listData !== undefined ? (
+        listData.map((value, index) => {
+          return (
+            <button
+              key={`${value}-${index * listData.length}-data`}
+              type="button"
+              style={{
+                display: `${show ? 'flex' : 'none'}`,
+              }}
+              className={`btn normal menu-item ${themeColor} ${index === selectedIdx ? 'selected' : ''}`}
+              onClick={() => {
+                selectIndex(index);
+                onSelect(!show);
+              }}
+            >
+              <span>{value}</span>
+            </button>
+          );
+        })
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+};
+interface DropDownToggleProps {
+  id: string;
+  text: string;
+  themeColor: string;
+}
+
+const DropDownToggle: React.FC<DropDownToggleProps> = ({ id, text, themeColor }) => {
+  const [props, { show }] = useDropdownToggle();
+  const [isFocusOutline, setFocusOutline] = useState(false);
+  return (
+    <button
+      type="button"
+      id={id}
+      {...props}
+      className={`dropdown-toggle tab-btn normal ${themeColor} ${!isFocusOutline ? 'no-focus-outline' : ''}`}
+      onKeyUp={(e) => {
+        if (e.key === 'Tab') {
+          setFocusOutline(true);
+        }
+      }}
+      onMouseUp={() => {
+        if (isFocusOutline) setFocusOutline(false);
+      }}
+    >
+      <span>{text}</span>
+      <div className={`toggle-icon ${show ? 'open' : ''}`}>
+        <CaretLeft themeColor={themeColor} />
+      </div>
+    </button>
+  );
+};
+
+interface DropDownButtonProps {
+  show: boolean;
+  onToggle: (nextShow: boolean, event?: React.SyntheticEvent<Element, Event> | undefined) => void;
+  drop: 'up' | 'down' | 'left' | 'right' | undefined;
+  alignEnd: boolean | undefined;
+  selectedIdx: number;
+  listData: string[];
+  themeColor: string;
+  selectIndex: (idx: number) => void;
+  onSelect: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const DropDownButton: React.FC<DropDownButtonProps> = ({
+  show,
+  onToggle,
+  drop,
+  alignEnd,
+  selectedIdx,
+  listData,
+  themeColor,
+  selectIndex,
+  onSelect,
+}) => (
+  <Dropdown show={show} onToggle={onToggle} drop={drop} alignEnd={alignEnd} itemSelector="button:not(:disabled)">
+    <div id="formation-dropdown">
+      <DropDownToggle
+        id="dropdown-toggle"
+        text={selectedIdx === undefined || listData.length === 0 ? 'Formations' : listData[selectedIdx]}
+        themeColor={themeColor}
+      />
+      <DropDownMenu
+        listData={listData}
+        themeColor={themeColor}
+        selectedIdx={selectedIdx}
+        selectIndex={selectIndex}
+        onSelect={onSelect}
+      />
+    </div>
+  </Dropdown>
+);
 
 const FormationDropDown: React.FC = () => {
   const dispatch = useDispatch();
@@ -13,9 +140,6 @@ const FormationDropDown: React.FC = () => {
   const selectIndex = (idx: number) => {
     dispatch(selectFormation(idx));
   };
-  useEffect(() => {
-    console.log('dropdown button show', show);
-  }, [show]);
   return (
     <DropDownButton
       show={show}
